2539b7840af0ce8200a28cde8d3ba2cff6f5ea9f
Santa dd <hethere97@gmail.com>
2020-10-11 08:09:18 +0000

    Add requirement.txt and Procfile

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
+import os
+from time import localtime, strftime
 from flask import Flask, render_template, redirect, url_for, flash
 from passlib.hash import pbkdf2_sha256
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 #Import the contents of model.py
 from models import *
 from wtforms_fields import *
@@ -9,13 +12,16 @@ from wtforms_fields import *
 
 #Create an Instance of the flask app
 app = Flask(__name__)
-app.secret_key = 'replace later'
+app.secret_key = os.environ.get('SECRET')
 
 #Configure the database
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://ffozzwshelgnck:f9e6a811c7287008929cecde47b04b206a4abf71f4b093418648fa5ac5cfef68@ec2-52-204-20-42.compute-1.amazonaws.com:5432/deellvnshen1t3'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL') 
 #Initialise the connection to our database
 db = SQLAlchemy(app)
-
+#Initialize Flask-SocketIO
+socketio = SocketIO(app)
+#Pre-define rooms
+ROOMS = ["lounge", "news", "games", "coding"]
 #Configure Flask Login
 login = LoginManager(app)
 login.init_app(app)
@@ -66,10 +72,10 @@ def login():
 
 #Accessible by logging in only
 @app.route("/chat", methods=['GET', 'POST'])
-@login_required
+#@login_required
 def chat():
 
-    return "Chat with me"
+    return render_template("chat.html", username=current_user.username, rooms=ROOMS)
 
 #Route to logout User
 @app.route("/logout", methods=["GET"])
@@ -78,6 +84,34 @@ def logout():
     flash("You have successfully logout", 'success')
     return redirect(url_for('login'))
 
+#Define event packages
+@socketio.on('message')
+def message(data):
+    #Broadcast message to other clients
+    print(f"\n\n{data}\n\n") 
+    send({'msg': data['msg'], 'username': data['username'], 'time_stamp': 
+         strftime('%b-%d %I:%M%p', localtime())}, room=data['room'])
+    #emit('some-event', 'this is a custom event message')
+
+#Join room route
+@socketio.on('join')
+def join(data):
+
+    join_room(data['room'])
+    #Notify clients when one joins the room
+    send({'msg': data['username'] + " has joined the " + data['room'] + " room."}, room=data['room'])
+
+@socketio.on('leave')
+def leave(data):
+    leave_room(data['room'])
+    send({'msg': data['username'] + "has left the " + data['room'] + " room."}, room=data['room'])
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
